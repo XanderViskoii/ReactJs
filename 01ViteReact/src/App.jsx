2ec5619ef041d71b4954007c2f48113ba88c3c54
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-18 23:12:36 +0000

    Curly brace syntax

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,16 +1,17 @@
 import  Chai from "./Chai";
 
 function App() {
-
+  const username = "chai aur code"
 
   return (
     <>
       <Chai />
-      <h1>React ball</h1>
+      <h1>React ball {username}</h1> 
       <b>Hum pagal</b>
     </>
     
   )
 }
+// Curly brace syntax : everything inside curly brace acts as a variable
 
 export default App
