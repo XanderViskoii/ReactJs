3a2c61b692c20c2e420b1bbbc9273a430d26c53d
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-18 00:17:34 +0000

    some changes in vite

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <>
+        <div>
+            <h1>Custom app !</h1>
+        </div>
+         </>
+    )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
+   <>
+    <MyApp />
     <App />
+   </>
+//   MyApp()
   
 )
