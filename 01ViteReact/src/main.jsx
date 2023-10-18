ab560d4f1475bafdba2fe9f8df1df5f285d9dfe4
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-18 23:12:58 +0000

    React element syntax

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -12,12 +12,41 @@ function MyApp(){
     )
 }
 
+// const ReactElement = {
+//     type : 'a',
+//     props : {
+//         href: 'https://www.amazon.in',
+//         target : '_blank'
+//     },
+//     children: 'Click me to visit amazon'
+// }
+
+const anotherUser = "Xander Viskoii"
+
+const ReactElement = React.createElement( //Syntax of React is predefined
+    'a', //tag
+    {href: "https://www.google.com", target: "_blank"}, //Giving properties in object without name
+    'Click me to visit google', //Writing text
+    anotherUser
+)
+
+
+
+const anotherElement = (
+    <a href="https://www.google.in" target='_blank'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-   <>
-    <MyApp />
-    <App />
-   </>
-//   MyApp()
+//    anotherElement,
+   ReactElement
+//    <> 
+//      <MyApp />,
+//      <App /> 
+//   </> 
+   
+//    MyApp()
+//    ReactElement
+    
   
 )
