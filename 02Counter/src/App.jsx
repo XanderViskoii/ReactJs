5cde50ac9796d4b42011299c7d1144ead3fa460e
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-19 23:43:55 +0000

    creation of reload btn & max limit for + - btn

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  let counter = 15
+  let [counter, setCounter] = useState(0) //At 0th index we get counter(name doesn't matter) ,at 1st index we get a function 
+
+  // let counter = 15
 
   const addValue = () => {
-    console.log("Clicked", counter);
+    // console.log("Clicked", counter);
     counter = counter + 1
+    if(counter <= 20){
+      setCounter(counter);
+    }
+    else{
+      // counter = 20;
+      console.log(`clicked`);
+      reload()
+    }
     
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+    else{
+      // setCounter(counter = 0)
+      console.log(`Clicked`);
+      reload()
+    }
+  }
+
+  const reload = () => {
+    setCounter(counter = 0 )
   }
 
   return (
@@ -20,7 +45,8 @@ function App() {
       <button type="submit" onClick={addValue}>Add value{counter}</button>
       <br />
       <br />
-      <button type="submit">Remove value{counter}</button>
+      <button type="submit" onClick={removeValue}>Remove value{counter}</button>
+      <button type="submit" onClick={reload}>Reload {counter}</button>
       <footer>footer : {counter}</footer>
     </>
   )
