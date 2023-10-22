dfcb5d8cf2820849818bba7cdaf43be6135af151
XanderViskoii <144394263+XanderViskoii@users.noreply.github.com>
2023-10-22 23:32:14 +0000

    d1

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,12 +7,17 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+     username : 'Xander Viskoii',
+   age : 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
       <h1 className='bg-gray-700 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <h2 className='bg-blue-300 text-bold p-5 rounded-xl mb-3 '>Use of Svg to be learned</h2>
-      <Card channel="XanderViskoii"/>
+      <h2 className='bg-yellow-900 text-green p-5 rounded-xl mb-3 '>Use of Svg to be learned</h2>
+      <Card  username="XanderViskoii" />
       <Card/>
     </>
   )
